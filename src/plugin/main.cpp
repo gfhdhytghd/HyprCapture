c31325e4db8dc7cffc4e800c4997996ceaaf9a4f
gfhdhytghd <3391146750@qq.com>
2026-06-20 04:29:30 +0800

    chore: bump version to 0.2.2

--- src/plugin/main.cpp
+++ src/plugin/main.cpp
@@ -511,7 +511,7 @@ APICALL EXPORT PLUGIN_DESCRIPTION_INFO PLUGIN_INIT(HANDLE handle) {
         .name = "HyprCapture",
         .description = "Hyprland-only screenshot overlay",
         .author = "wilf",
-        .version = "0.2.1",
+        .version = "0.2.2",
     };
 }
 
